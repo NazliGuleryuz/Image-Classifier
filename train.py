724d2a23ec0eed2ba73a152e4ea6d6a6867cad0a
Nazli <34333457+NazliGuleryuz@users.noreply.github.com>
2020-07-15 13:09:02 +0300

    Add files via upload
    Developing an Image Classifier with Deep Learning
    In this first part of the project, you'll work through a Jupyter notebook to implement an image classifier with PyTorch. We'll provide some tips and guide you, but for the most part the code is left up to you. As you work through this project, please refer to the rubric for guidance towards a successful submission.
    
    Remember that your code should be your own, please do not plagiarize (see here for more information).
    
    This notebook will be required as part of the project submission. After you finish it, make sure you download it as an HTML file and include it with the files you write in the next part of the project.
    
    We've provided you a workspace with a GPU for working on this project. If you'd instead prefer to work on your local machine, you can find the files on GitHub here.
    
    If you are using the workspace, be aware that saving large files can create issues with backing up your work. You'll be saving a model checkpoint in Part 1 of this project which can be multiple GBs in size if you use a large classifier network. Dense networks can get large very fast since you are creating N x M weight matrices for each new layer. In general, it's better to avoid wide layers and instead use more hidden layers, this will save a lot of space. Keep an eye on the size of the checkpoint you create. You can open a terminal and enter ls -lh to see the sizes of the files. If your checkpoint is greater than 1 GB, reduce the size of your classifier network and re-save the checkpoint.
    Part 2 - Building the command line application
    Now that you've built and trained a deep neural network on the flower data set, it's time to convert it into an application that others can use. Your application should be a pair of Python scripts that run from the command line. For testing, you should use the checkpoint you saved in the first part.
    
    Specifications
    The project submission must include at least two files train.py and predict.py. The first file, train.py, will train a new network on a dataset and save the model as a checkpoint. The second file, predict.py, uses a trained network to predict the class for an input image. Feel free to create as many other files as you need. Our suggestion is to create a file just for functions and classes relating to the model and another one for utility functions like loading data and preprocessing images. Make sure to include all files necessary to run train.py and predict.py in your submission.
    
    Train a new network on a data set with train.py
    
    Basic usage: python train.py data_directory
    Prints out training loss, validation loss, and validation accuracy as the network trains
    Options:
    Set directory to save checkpoints: python train.py data_dir --save_dir save_directory
    Choose architecture: python train.py data_dir --arch "vgg13"
    Set hyperparameters: python train.py data_dir --learning_rate 0.01 --hidden_units 512 --epochs 20
    Use GPU for training: python train.py data_dir --gpu
    Predict flower name from an image with predict.py along with the probability of that name. That is, you'll pass in a single image /path/to/image and return the flower name and class probability.
    
    Basic usage: python predict.py /path/to/image checkpoint
    Options:
    Return top KK most likely classes: python predict.py input checkpoint --top_k 3
    Use a mapping of categories to real names: python predict.py input checkpoint --category_names cat_to_name.json
    Use GPU for inference: python predict.py input checkpoint --gpu
    The best way to get the command line input into the scripts is with the argparse module in the standard library. You can also find a nice tutorial for argparse here.

---
+++ train.py
@@ -0,0 +1,217 @@
+import argparse
+import os
+import time
+import torch
+import matplotlib.pyplot as plt 
+import numpy as np
+import json
+
+from torch import nn, optim
+from torchvision import datasets, models, transforms
+from collections import OrderedDict
+from PIL import Image
+
+ 
+def def_data(data_dir):
+    print("processing data into training data, test data, validation data and labels")
+    train_dir, test_dir, valid_dir = data_dir 
+    train_transforms = transforms.Compose([transforms.RandomRotation(30),
+                                       transforms.RandomResizedCrop(224),
+                                       transforms.RandomHorizontalFlip(),
+                                       transforms.ToTensor(),
+                                       transforms.Normalize([0.485, 0.456, 0.406], 
+                                                            [0.229, 0.224, 0.225])])
+
+    valid_transforms = transforms.Compose([transforms.Resize(256),
+                                      transforms.CenterCrop(224),
+                                      transforms.ToTensor(),
+                                      transforms.Normalize([0.485, 0.456, 0.406], 
+                                                           [0.229, 0.224, 0.225])])
+    test_transforms = transforms.Compose([transforms.Resize(256),
+                                      transforms.CenterCrop(224),
+                                      transforms.ToTensor(),
+                                      transforms.Normalize([0.485, 0.456, 0.406], 
+                                                           [0.229, 0.224, 0.225])])
+    train_data = datasets.ImageFolder(train_dir, transform=train_transforms)
+    valid_data = datasets.ImageFolder(valid_dir , transform=valid_transforms)
+    test_data = datasets.ImageFolder(test_dir, transform=test_transforms)
+
+
+    # Data batching
+    trainloader = torch.utils.data.DataLoader(train_data, batch_size=50, shuffle=True)
+    validloader = torch.utils.data.DataLoader(valid_data, batch_size=50)
+    testloader = torch.utils.data.DataLoader(test_data, batch_size=50)
+    with open('cat_to_name.json', 'r') as f:
+        cat_to_name = json.load(f)
+    loaders = {'train':trainloader,'valid':validloader,'test':testloader,'labels':cat_to_name}
+    return loaders
+
+def getdata():
+    print("rooting data")
+    train_dir = args.data_directory + '/train'
+    test_dir = args.data_directory + '/test'
+    valid_dir = args.data_directory + '/valid'
+    data_dir = [train_dir,test_dir,valid_dir]
+    return def_data(data_dir)
+
+def specify_model(data):
+    print("model specification")
+    if (args.arch is None):
+        arch_type = 'vgg16'
+        model = models.vgg16(pretrained=True)
+        
+       
+        model.name = "vgg16"
+
+        
+        print("Network architecture specified as vgg16.")
+        
+    else:
+        arch_type = args.arch
+    for param in model.parameters():
+        param.requires_grad = False      
+        
+    classifier = nn.Sequential(OrderedDict([
+                              ('fc1', nn.Linear(25088, 4096, bias=True)),#first layer linear
+                              ('relu1', nn.ReLU()),#first layer is using relu activation function
+                              ('dropout1', nn.Dropout(p=0.5)),#again, dropout value received from model above
+                              ('fc2',nn.Linear(4096, 4096, bias=True)),
+                              ('reLu2',nn.ReLU()),
+                              ('dropout2',nn.Dropout(p=0.5)),
+                              ('fc3', nn.Linear(4096, 102, bias=True)),#thirdlayer is softmax
+                              ('output', nn.LogSoftmax(dim=1))]))#batch size dim 0 and actual vector passing through is                            #notice how output is 102 instead of 1000 because thats how many pics we have
+
+    model.classifier = classifier
+    criterion = nn.NLLLoss()
+    optimizer = optim.Adam(model.classifier.parameters(), lr=0.001)
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    model.to(device);
+    
+    return model
+
+def validation(model, testloader, criterion): 
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    model.to(device);
+    test_loss = 0
+    accuracy = 0
+    criterion = nn.NLLLoss()
+    optimizer = optim.Adam(model.classifier.parameters(), lr=0.001)    
+    
+    for images, labels in testloader:    
+        images, labels = images.to(device), labels.to(device)
+        
+        output = model.forward(images)
+        test_loss += criterion(output, labels).item()
+        
+        ps = torch.exp(output)#probsbility
+        equality = (labels.data == ps.max(dim=1)[1])
+        accuracy += equality.type(torch.FloatTensor).mean()
+    
+    return test_loss, int(accuracy)
+
+def train(model,data):
+    print("training model")
+    
+    print_every=30
+    steps = 0
+    epochs = 0
+    if (args.gpu):
+        device = 'cuda'
+    else:
+        device = 'cpu'
+    
+
+    
+    trainloader=data['train']
+    validloader=data['valid']
+    testloader=data['test']
+    
+    criterion = nn.NLLLoss()
+    optimizer = optim.Adam(model.classifier.parameters(), lr=0.001)
+
+    model.to(device)
+    
+    for e in range(epochs):
+        running_loss = 0 #doing this to calculate loss during training
+        model.train() # Technically not necessary, setting this for good measure
+
+
+        for images, labels in iter(trainloader):
+            steps += 1
+
+            images, labels = images.to(device), labels.to(device)
+
+            optimizer.zero_grad()#Clears the gradients, do this because gradients are accumulated
+
+            # Forward and backward passes
+            outputs = model.forward(images)#passing our images
+            loss = criterion(outputs, labels)#calculated loss necessary to calculate the gradient
+            loss.backward()#calculates the gradients
+            optimizer.step()#then with the gradients you can take a step to update the weights
+
+            running_loss += loss.item()
+
+            if steps % print_every == 0:
+                model.eval()#turns dropout off
+
+                with torch.no_grad():
+                    valid_loss, accuracy = validation(model, validloader, criterion)
+            
+                print("Epoch: {}/{} | ".format(e+1, epochs),
+                  "Training Loss: {:.5f} | ".format(running_loss/print_every),
+                  "Validation Loss: {:.5f} | ".format(valid_loss/len(testloader)),
+                  "Validation Accuracy: {:.5f}".format(accuracy/len(testloader)))
+                running_loss = 0
+                model.train()#turns dropout back on
+
+    print("\nTraining process is completed.")
+    
+    test_result = validation(model,testloader,device)
+    print('test loss and accuracy of the test setis respectively : {}'.format(test_result))
+    return model
+
+def save(model):
+    print("saving model")
+    if (args.save_dir is None):
+        save_dir = 'check.pth'
+    else:
+        save_dir = args.save_dir
+    checkpoint = {
+                'model': model.cpu(),
+                'features': model.features,
+                'classifier': model.classifier,
+                'state_dict': model.state_dict()}
+    torch.save(checkpoint, save_dir)
+    return 0
+def create_model():
+
+    data =getdata()
+    model = specify_model(data)
+    model = train(model,data)
+    save(model)
+    return None
+
+def parse():
+    parser = argparse.ArgumentParser(description='Train a neural network with open of many options!')
+    parser.add_argument('data_directory', help='data directory (required)')
+    parser.add_argument('--save_dir', help='directory to save a neural network.')
+    parser.add_argument('--arch', help='models to use OPTIONS[vgg,densenet]')
+    parser.add_argument('--learning_rate', help='learning rate')
+    parser.add_argument('--hidden_units', help='number of hidden units')
+    parser.add_argument('--epochs', help='epochs')
+    parser.add_argument('--gpu',action='store_true', help='gpu')
+    args = parser.parse_args()
+    return args
+
+def main():
+
+
+   
+    print("creating an image classifier")
+    global args
+    args = parse()
+    create_model()
+    print("model finished!")
+    return None
+
+main()
